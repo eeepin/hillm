9832df89a9b8ad936a01ae1d55302d4f04cbc52a
Eepin <eepin.wu@gmail.com>
2026-07-05 17:43:40 +0800

    feat: make sure the rustls crypto provider installed

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,11 @@ pub mod image;
 pub mod provider;
 /// Requests/Response Data Transfer Objects.
 pub mod types;
+
+pub fn ensure_crypto_provider() {
+    use std::sync::Once;
+    static INIT: Once = Once::new();
+    INIT.call_once(|| {
+        let _ = rustls::crypto::ring::default_provider().install_default();
+    });
+}
